cc00771c3601324f2aeed0c73bef2bf90e7eb3f7
Marcelo <113069760+K1nghthawk@users.noreply.github.com>
2022-09-24 18:08:30 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+let diaSemana = prompt("¿Qué día es hoy?");
+
+switch (diaSemana) {
+case "lunes":
+
+alert("Rutina Piernas: sentadillas + zancadas + cardio.");
+
+break;
+case "martes":
+
+alert("Rutina Pectorales: press banca + flexiones + press con mancuerna. ");
+
+break;
+case "miércoles":
+
+alert("Rutina Abdominales: crunch + plancha + elevaciones de pierna.");
+
+break;
+case "jueves":
+
+alert("Rutina Espalda: dominadas + peso muerto + remo con barra.");
+
+break;
+case "viernes":
+
+alert("Rutina Hombros: press militar + elevaciones frontales y laterales + flys invertidos.");
+
+break;
+case "sábado":
+
+alert("Rutina Biceps/Triceps: curl con barra + curl martillo + press frances + fondos de banca.");
+
+break;
+case "domingo":
+
+alert("Dia de descanso");
+
+break;
+default:
+
+alert("Escribe el día de la semana correctamente y en minúsculas.");
+
+}
